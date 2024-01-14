43c647e06692e54d1707c25d839a19e2f1f62fb0
sakshi <sakshirana765@gmail.com>
2024-01-14 20:21:32 +0530

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer =() => {
+    return(
+        <div className="bg-accent text-white p-4 text-center mt-16"> &copy; Copyright | All Rights Reserved <br/> By Sakshi</div>
+    )
+}
+
+export default Footer;
